5d68329ffee7f27db908ffcfde2842572cab63f3
noahhusby <32528627+noahhusby@users.noreply.github.com>
2024-08-15 14:49:19 -0400

    refact: setup project structure

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+        name='aiostreammagic',
+        version='0.0.1',
+        packages=['aiostreammagic'],
+        license='MIT',
+        author='Noah Husby',
+        url='https://github.com/noahhusby/aiostreammagic',
+        description='An async python package for interfacing with Cambridge Audio / Stream Magic compatible streamers',
+        long_description=open('README.md').read(),
+        long_description_content_type='text/markdown',
+
+)
