e1dadc86b7699706ba419a0f966688f81d4f32fb
Solmath <thommy86@gmail.com>
2025-07-20 11:18:35 +0200

    feat: refactor equalizer methods to improve clarity and add band filter functionality

--- aiostreammagic/stream_magic.py
+++ aiostreammagic/stream_magic.py
@@ -629,29 +629,39 @@ class StreamMagicClient:
         """Sets whether the internal equalizer is enabled."""
         await self.request(ep.AUDIO, params={"zone": "ZONE1", "user_eq": enabled})
 
-    async def set_equalizer_frequency(self, band_index: int, frequency: int) -> None:
+    async def set_equalizer_band_filter(
+        self, band_index: int, filter_type: EQFilterType
+    ) -> None:
+        """Sets the filter type for a specific equalizer band."""
+        band = EQBand(index=band_index, filter=filter_type)
+        eq = UserEQ(enabled=None, bands=[band])
+        await self.set_equalizer_params(eq)
+
+    async def set_equalizer_band_frequency(
+        self, band_index: int, frequency: int
+    ) -> None:
         """Sets the frequency for a specific equalizer band."""
         if not 20 <= frequency <= 20000:
             raise StreamMagicError("Frequency must be between 20 Hz and 20 kHz")
         band = EQBand(index=band_index, freq=frequency)
-        eq = UserEQ(None, [band])
-        await self.set_equalizer(eq)
+        eq = UserEQ(enabled=None, bands=[band])
+        await self.set_equalizer_params(eq)
 
-    async def set_equalizer_gain(self, band_index: int, gain: float) -> None:
+    async def set_equalizer_band_gain(self, band_index: int, gain: float) -> None:
         """Sets the gain for a specific equalizer band."""
         if not -6 <= gain <= 3:
             raise StreamMagicError("Gain must be between -6 dB and 3 dB")
         band = EQBand(index=band_index, gain=gain)
-        eq = UserEQ(None, [band])
-        await self.set_equalizer(eq)
+        eq = UserEQ(enabled=None, bands=[band])
+        await self.set_equalizer_params(eq)
 
-    async def set_equalizer_q_factor(self, band_index: int, q: float) -> None:
+    async def set_equalizer_band_q_factor(self, band_index: int, q: float) -> None:
         """Sets the Q factor for a specific equalizer band."""
         if not 0.1 <= q <= 10:
             raise StreamMagicError("Q factor must be between 0.1 and 10")
         band = EQBand(index=band_index, q=q)
-        eq = UserEQ(None, [band])
-        await self.set_equalizer(eq)
+        eq = UserEQ(enabled=None, bands=[band])
+        await self.set_equalizer_params(eq)
 
     async def set_equalizer_defaults(self) -> None:
         """Sets the equalizer to the default settings."""
@@ -664,10 +674,10 @@ class StreamMagicClient:
             EQBand(index=5, filter=EQFilterType.PEAKING, freq=5000, gain=0.0, q=1.24),
             EQBand(index=6, filter=EQFilterType.HIGHSHELF, freq=8000, gain=0.0, q=0.8),
         ]
-        eq = UserEQ(enabled=True, bands=bands)
-        await self.set_equalizer(eq)
+        eq = UserEQ(enabled=None, bands=bands)
+        await self.set_equalizer_params(eq)
 
-    async def set_equalizer(self, settings: UserEQ) -> None:
+    async def set_equalizer_params(self, settings: UserEQ) -> None:
         """Sets the internal equalizer to the provided settings"""
 
         await self.request(
